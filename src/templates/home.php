885f52aabd2f0377822f64ca5aa7607e4c6a81c3
Gabriel Domeneghetti de Barros <gabriel.barros@hotmail.com>
2022-01-21 23:15:51 -0300

    Create and join order

--- src/templates/home.php
+++ src/templates/home.php
@@ -11,45 +11,12 @@ include "config.php";
 </div>
             
 <div class="row">
-  <h2 class="col-xs-12 col-lg-10 col-lg-offset-1">Join or Create a session:</h2>
-  
-  <!-- Join session panel -->
-  <div class="col-xs-12 col-sm-6 col-lg-5 col-lg-offset-1" ng-controller="JoinController as join">
-    <div class="panel panel-default">
-      <div class="panel-heading">Join session</div>
-      <div class="panel-body">
-        <form role="form">
-          <div class="form-group" ng-class="{'has-error': join.idError}">
-            <label>Session Number:</label>
-            <div class="has-feedback">
-              <input type="text" class="form-control" ng-model="join.id" ng-change="join.passwordCheck()" placeholder="1234">
-              <span ng-if="join.idError" class="glyphicon glyphicon-remove form-control-feedback"></span>
-            </div>
-          </div>
-          <div class="form-group" ng-class="{'has-error': join.nameError}">
-            <label>Your Name:</label>
-            <div class="has-feedback" ng-init="join.name = '<?= isset($_COOKIE['scrum_member_name']) ? $_COOKIE['scrum_member_name'] : "" ?>'">
-              <input type="text" class="form-control"  ng-model="join.name" placeholder="John">
-              <span ng-if="join.nameError" class="glyphicon glyphicon-remove form-control-feedback"></span>
-            </div>
-          </div>
-          <div class="form-group" ng-if="join.requiresPassword">
-            <label>Password:</label>
-            <div class="has-feedback">
-              <input type="password" class="form-control"  ng-model="join.password">
-              <span ng-if="join.passwordError" class="glyphicon glyphicon-remove form-control-feedback"></span>
-            </div>
-          </div>
-          <input type="button" class="btn btn-default" value="Join" ng-click="join.joinSession()">
-       </form>
-      </div>
-    </div>        
-  </div>
+  <h2 class="col-xs-12 col-lg-10 col-lg-offset-1">Create or join a session:</h2>
   
   <!-- Create session panel -->
   <div class="col-xs-12 col-sm-6 col-lg-5" ng-controller="CreateController as create">
     <div class="panel panel-default">
-      <div class="panel-heading">Create session</div>
+      <div class="panel-heading">Create</div>
       <div class="panel-body">  
         <form role="form">
           <div class="form-group" ng-class="{'has-error': create.nameError}">
@@ -85,6 +52,39 @@ include "config.php";
           </div>
           <input type="button" class="btn btn-default" value="Create" ng-click="create.createSession()">
         </form>
+      </div>
+    </div>        
+  </div>
+
+  <!-- Join session panel -->
+  <div class="col-xs-12 col-sm-6 col-lg-5 col-lg-offset-1" ng-controller="JoinController as join">
+    <div class="panel panel-default">
+      <div class="panel-heading">Join</div>
+      <div class="panel-body">
+        <form role="form">
+          <div class="form-group" ng-class="{'has-error': join.idError}">
+            <label>Session Number:</label>
+            <div class="has-feedback">
+              <input type="text" class="form-control" ng-model="join.id" ng-change="join.passwordCheck()" placeholder="1234">
+              <span ng-if="join.idError" class="glyphicon glyphicon-remove form-control-feedback"></span>
+            </div>
+          </div>
+          <div class="form-group" ng-class="{'has-error': join.nameError}">
+            <label>Your Name:</label>
+            <div class="has-feedback" ng-init="join.name = '<?= isset($_COOKIE['scrum_member_name']) ? $_COOKIE['scrum_member_name'] : "" ?>'">
+              <input type="text" class="form-control"  ng-model="join.name" placeholder="John">
+              <span ng-if="join.nameError" class="glyphicon glyphicon-remove form-control-feedback"></span>
+            </div>
+          </div>
+          <div class="form-group" ng-if="join.requiresPassword">
+            <label>Password:</label>
+            <div class="has-feedback">
+              <input type="password" class="form-control"  ng-model="join.password">
+              <span ng-if="join.passwordError" class="glyphicon glyphicon-remove form-control-feedback"></span>
+            </div>
+          </div>
+          <input type="button" class="btn btn-default" value="Join" ng-click="join.joinSession()">
+       </form>
       </div>
     </div>        
   </div>
